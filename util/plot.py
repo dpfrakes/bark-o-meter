0aef6be704f86160256664a7beffcd915df8d76b
Dan Frakes <dpfrakes@gmail.com>
2019-08-11 13:12:34 -0400

    hotfix

--- util/plot.py
+++ util/plot.py
@@ -8,5 +8,5 @@ def plot_signal(frames, seconds, occasion):
     timesteps = [1. * f / seconds for f in range(len(frames))]
     plt.fill_between(timesteps, frames, color='lightblue')
     plt.fill_between(timesteps, 0 - frames, color='lightblue')
-    plt.savefig(os.path.join('output', 'output_{}.png'.format()))
+    plt.savefig(os.path.join('output', 'output_{}.png'.format(occasion)))
     plt.show()
